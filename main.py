03e9c47f37d45077bb9baea19e013cef1df91b5c
ungsungi <yangyesungcute@gmail.com>
2026-05-18 10:10:29 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,5 +5,5 @@ b=st.selectbox('좋아하는 음식을 선택하세요',['치킨','마라탕','�
 if st.button('인사말 생성'):
   st.write(a+'님, 안녕하세요')
   st.info('반갑습니당')
-  st.warning(b+'이 음식을 좋아하나봐긔?')
+  st.warning(b+'이거 좋아하나봐긔?')
   st.error('요로시쿠오네가이시마스')
